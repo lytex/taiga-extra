519db2de8eb7fe7f2b9f41900db4e12c43cbd775
Julián López Carballal <julopezc@acciona.com>
2020-02-24 22:47:05 +0100

    kanban.py was duplicated in inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -13,32 +13,28 @@ project = api.projects.get_by_slug(c.PROJECT_SLUG)
 
 print('Getting user stories ...', flush=True)
 user_stories = api.user_stories.list(project=project.id)
+user_story_dict_inv = {x.name: x.id for x in project.list_user_story_statuses()}
 
-current_sprint = list(filter(lambda x: not x.closed, project.milestones))[0]
-
-current_user_stories = list(filter(lambda x: x.milestone == current_sprint.id, user_stories))
-
-tag = 'current_sprint'
-
-
-for story in current_user_stories:
-    story_with_tag = list(filter(lambda x: x[0] == tag, story.tags))
-    if not story_with_tag:
-        story.tags.append([tag, None])
-        story.update()    
+print('Getting tasks ...', flush=True)
+tasks = api.tasks.list(project=project.id)
+task_dict = {x.id: x.name for x in project.list_task_statuses()}
 
 
-old_user_stories = list(filter(lambda x: x.milestone != current_sprint.id, user_stories))
+status = [x.name for x in project.list_task_statuses()]
+status2 = [x.name for x in project.list_user_story_statuses()]
+assert status == status2, "Task statuses and user statuses must be the same"
 
-for story in old_user_stories:
-    story_with_tag = list(filter(lambda x: x[0] == tag, story.tags))
-    if story_with_tag:
-        print(story.tags)
-        story.tags.remove([tag, None])
-        story.update()
-        print(story.tags)
+for story in user_stories:
+    # Assume statuses are ordered from "less" done to "more" done
+    index = len(status)-1
+    for task in tasks:
+        if task.user_story == story.id:
+            next_index = status.index(task_dict[task.status])
+            if next_index < index:
+                index = next_index
+                
+    story.status = user_story_dict_inv[status[index]]
+    story.update()
 
 
-
-
-# ejemplo de tags: [['urgencias', '#f57900'], ['prueba1', None]]
+    
